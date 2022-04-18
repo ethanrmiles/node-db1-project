c4cb87bd0fe68cd65f3e377e64eee8dc067c27e6
Ethan Miles <ethanmilescs@gmail.com>
2022-04-18 14:29:29 -0500

    Added the query by id model

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,57 @@
 const router = require('express').Router()
+const middleware = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'good job! you found me!'})
+router.get('/', async(req, res, next) => {
+  try{
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  }catch (err){
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  res.status(200).json({ message: 'good job! you found me!'})
+router.get('/:id',  middleware.checkAccountId, async (req, res, next) => {
+   try{
+    const account = await Account.getById(req.params.id)
+    res.json(account)
+  }catch (err){
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  res.status(200).json({ message: 'good job! you found me!'})
+router.post('/', 
+middleware.checkAccountPayload, 
+middleware.checkAccountNameUnique, 
+(req, res, next) => {
+    try{
+    res.json('post accounts')
+  }catch (err){
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  res.status(200).json({ message: 'good job! you found me!'})
+router.put('/:id', 
+middleware.checkAccountId, 
+middleware.checkAccountPayload,
+(req, res, next) => {
+  try{
+    res.json('update accounts at a given id')
+  }catch (err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({ message: 'good job! you found me!'})
+router.delete('/:id', middleware.checkAccountId, (req, res, next) => {
+   try{
+    res.json('delete accounts at a given id')
+  }catch (err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(200).json({ message: 'good job! you found me!'})
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = router;
