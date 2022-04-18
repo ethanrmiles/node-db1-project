f2fc246f4f4f7714cccc597c98247f1d332ed7d1
Ethan Miles <ethanmilescs@gmail.com>
2022-04-18 14:06:03 -0500

    routers are routed and sending data to the client

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,27 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(200).json({ message: 'good job! you found me!'})
 })
 
 module.exports = router;
